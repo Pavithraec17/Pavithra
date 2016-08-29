3ac190e3df43c3d5c71af7aefdb763cb4862675f
Pavithraec17 <pavithrap017@gmail.com>
2016-08-29 14:16:30 +0530

    Create begin1.c

---
+++ begin1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int a;
+ clrscr();
+ printf("\nEnter the number:");
+ scanf("%d",&a);
+ if(a!=0)
+ {
+   if(a>0):
+   printf("\nPositive number");
+   else if:
+   printf("\nNegative number");
+   else
+   printf("\nThe number is zero");
+  }
+  getch();
+ }
